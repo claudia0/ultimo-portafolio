9ae7f89170a2122924af5e2c1d67ebdfa9e3f05b
claudia0 <reyesclaudiav@gmail.com>
2016-12-02 17:15:32 -0600

    arrglando menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $( document ).ready(function(){
 
-$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav();
 	
+	$('.btnSobreMi').click(function() {
+		window.location = "#sobreMi";
+		$(".button-collapse").sideNav()
+	});
+
+	$('.btnWeb').click(function() {
+		window.location = "#web";
+		$(".button-collapse").sideNav()
+	});
+
+	$('.btnContacto').click(function() {
+		window.location = "#contacto";
+		$(".button-collapse").sideNav()
+	});
     
+    $('.btnProyectos').click(function() {
+		window.location = "#proyectos";
+		$(".button-collapse").sideNav()
+	});
  
 });
   
